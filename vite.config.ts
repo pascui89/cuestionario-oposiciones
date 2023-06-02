cb8fb0e9b88a3beff0986385fd25669691b830bd
pascui89 <pascui89@gmail.com>
2023-06-02 10:58:00 +0200

    Included vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel(), vercelSsr()],
   base: "/cuestionario-oposiciones/"
 })
